2799856a818260f79afa73cfc4932e52077f1d39
Kevin <kevinsteven0513@gmail.com>
2025-02-03 16:03:49 -0500

    Avance regular sobre el componente Newsx

--- X-card/src/App.jsx
+++ X-card/src/App.jsx
@@ -1,13 +1,34 @@
 import { CardxFollow } from "./components/CardxFollow";
 import "./App.css";
+import { Newsx } from "./components/Newsx";
 
 function App() {
   return (
     <>
-      <div className="container-cardApp">
-        <CardxFollow isFollowing={true} name='imantado' username='imantadotv' />
-        <CardxFollow isFollowing name='Miguel Angel Duran' username='midudev' />
-        <CardxFollow isFollowing={false} name='auronPlay' username='auronplay' />
+      <div className="container-app">
+        <div className="container-news">
+          <Newsx type='Music - Trending' title='Kanye' post="443K posts"/>
+          <Newsx type='Trending in Colombia' title='#GRAMMYs' post="4.8M posts"/>
+          <Newsx type='Manchester United - Trending' title='Cristiano Ronaldo' post="50.4K posts"/>
+          <Newsx type='Sports - Trending' title='Chelsea' post="152K posts"/>
+        </div>
+        <div className="container-cardApp">
+          <CardxFollow
+            isFollowing={true}
+            name="imantado"
+            username="imantadotv"
+          />
+          <CardxFollow
+            isFollowing
+            name="Miguel Angel Duran"
+            username="midudev"
+          />
+          <CardxFollow
+            isFollowing={false}
+            name="auronPlay"
+            username="auronplay"
+          />
+        </div>
       </div>
     </>
   );
